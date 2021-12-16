7783eb2b3463925ae1c658fc340e57a688294471
Justin Burnett <47673797+ZippyBurns@users.noreply.github.com>
2021-12-16 11:36:06 -0700

    Initial upload of working engine
    The mechanics are complete.   TODO:  Add Menu screen and incorporate start and finish points of the mazes.

---
+++ My_Doom/src/Texture.java
@@ -0,0 +1,37 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Texture {
+	public int[] pixels;
+	private String loc;
+	public final int SIZE;
+	
+	public static Texture wood = new Texture("./src/res/wood.png", 64);
+	public static Texture brick = new Texture("./src/res/brick.png", 64);
+	public static Texture bluestone = new Texture("./src/res/bluestone.png", 64);
+	public static Texture stone = new Texture("./src/res/stone.png", 64);
+
+	
+	public Texture(String location, int size) {
+		loc = location;
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		load();
+	}
+	
+	private void load() {
+		try {
+			BufferedImage image = ImageIO.read(new File(loc));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+//	ImageIO.read(getClass().getClassLoader().getResource("resources/icon.gif"));
+	
+}
